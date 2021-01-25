6bc7566838851e3a485e91f1416364ea02608e8a
jeffresh <manuel.pallanunez@alum.uca.es>
2021-01-25 14:57:45 +0100

    refactor(config):  improve format and comments

--- section4/procedural_programing/house_prices_demo/config.py
+++ section4/procedural_programing/house_prices_demo/config.py
@@ -1,17 +1,17 @@
-# ================== Paths
+# ====   PATHS ===================
 
 PATH_TO_DATASET = "houseprice.csv"
 OUTPUT_SCALER_PATH = 'scaler.pkl'
 OUTPUT_MODEL_PATH = 'lasso_regression.pkl'
 
-# =================== Parameters
 
-# Imputation parameters
+# ======= PARAMETERS ===============
 
+# imputation parameters
 LOTFRONTAGE_MODE = 60
 
-# Encoding parameters
 
+# encoding parameters
 FREQUENT_LABELS = {
     'MSZoning': ['FV', 'RH', 'RL', 'RM'],
     'Neighborhood': ['Blmngtn', 'BrDale', 'BrkSide', 'ClearCr', 'CollgCr',
@@ -29,56 +29,55 @@ FREQUENT_LABELS = {
     'FireplaceQu': ['Ex', 'Fa', 'Gd', 'Missing', 'Po', 'TA'],
     'GarageType': ['Attchd', 'Basment', 'BuiltIn', 'Detchd', 'Missing'],
     'GarageFinish': ['Fin', 'Missing', 'RFn', 'Unf'],
-    'PavedDrive': ['N', 'P', 'Y']
-}
+    'PavedDrive': ['N', 'P', 'Y']}
 
-ENCODING_MAPPINGS = {
-    'MSZoning': {'Rare': 0, 'RM': 1, 'RH': 2, 'RL': 3, 'FV': 4},
-    'Neighborhood': {'IDOTRR': 0, 'MeadowV': 1, 'BrDale': 2, 'Edwards': 3,
-                     'BrkSide': 4, 'OldTown': 5, 'Sawyer': 6, 'SWISU': 7, 'NAmes': 8,
-                     'Mitchel': 9, 'SawyerW': 10, 'Rare': 11, 'NWAmes': 12, 'Gilbert': 13,
-                     'Blmngtn': 14, 'CollgCr': 15, 'Crawfor': 16, 'ClearCr': 17, 'Somerst': 18,
-                     'Timber': 19, 'StoneBr': 20, 'NridgHt': 21, 'NoRidge': 22},
-    'RoofStyle': {'Gable': 0, 'Rare': 1, 'Hip': 2},
-    'MasVnrType': {'None': 0, 'Rare': 1, 'BrkFace': 2, 'Stone': 3},
-    'BsmtQual': {'Missing': 0, 'Fa': 1, 'TA': 2, 'Gd': 3, 'Ex': 4},
-    'BsmtExposure': {'Missing': 0, 'No': 1, 'Mn': 2, 'Av': 3, 'Gd': 4},
-    'HeatingQC': {'Rare': 0, 'Fa': 1, 'TA': 2, 'Gd': 3, 'Ex': 4},
-    'CentralAir': {'N': 0, 'Y': 1},
-    'KitchenQual': {'Fa': 0, 'TA': 1, 'Gd': 2, 'Ex': 3},
-    'FireplaceQu': {'Po': 0, 'Missing': 1, 'Fa': 2, 'TA': 3, 'Gd': 4, 'Ex': 5},
-    'GarageType': {'Missing': 0, 'Rare': 1, 'Detchd': 2, 'Basment': 3, 'Attchd': 4, 'BuiltIn': 5},
-    'GarageFinish': {'Missing': 0, 'Unf': 1, 'RFn': 2, 'Fin': 3},
-    'PavedDrive': {'N': 0, 'P': 1, 'Y': 2}
-}
 
-# =================== Feature groups
+ENCODING_MAPPINGS = {'MSZoning': {'Rare': 0, 'RM': 1, 'RH': 2, 'RL': 3, 'FV': 4},
+                     'Neighborhood': {'IDOTRR': 0, 'MeadowV': 1, 'BrDale': 2, 'Edwards': 3,
+                                      'BrkSide': 4, 'OldTown': 5, 'Sawyer': 6, 'SWISU': 7, 'NAmes': 8,
+                                      'Mitchel': 9, 'SawyerW': 10, 'Rare': 11, 'NWAmes': 12, 'Gilbert': 13,
+                                      'Blmngtn': 14, 'CollgCr': 15, 'Crawfor': 16, 'ClearCr': 17, 'Somerst': 18,
+                                      'Timber': 19, 'StoneBr': 20, 'NridgHt': 21, 'NoRidge': 22},
+                     'RoofStyle': {'Gable': 0, 'Rare': 1, 'Hip': 2},
+                     'MasVnrType': {'None': 0, 'Rare': 1, 'BrkFace': 2, 'Stone': 3},
+                     'BsmtQual': {'Missing': 0, 'Fa': 1, 'TA': 2, 'Gd': 3, 'Ex': 4},
+                     'BsmtExposure': {'Missing': 0, 'No': 1, 'Mn': 2, 'Av': 3, 'Gd': 4},
+                     'HeatingQC': {'Rare': 0, 'Fa': 1, 'TA': 2, 'Gd': 3, 'Ex': 4},
+                     'CentralAir': {'N': 0, 'Y': 1},
+                     'KitchenQual': {'Fa': 0, 'TA': 1, 'Gd': 2, 'Ex': 3},
+                     'FireplaceQu': {'Po': 0, 'Missing': 1, 'Fa': 2, 'TA': 3, 'Gd': 4, 'Ex': 5},
+                     'GarageType': {'Missing': 0, 'Rare': 1, 'Detchd': 2, 'Basment': 3, 'Attchd': 4, 'BuiltIn': 5},
+                     'GarageFinish': {'Missing': 0, 'Unf': 1, 'RFn': 2, 'Fin': 3},
+                     'PavedDrive': {'N': 0, 'P': 1, 'Y': 2}}
+
+
+# ======= FEATURE GROUPS =============
 
 # variable groups for engineering steps
-
 TARGET = 'SalePrice'
 
-CATEGORICAL_TO_IMPUTE = ['MasVnrType', 'BSmtQual',
-                         'BsmtExposure', 'FireplaceQu', 'GarageType', 'GarageFinish']
+CATEGORICAL_TO_IMPUTE = ['MasVnrType', 'BsmtQual', 'BsmtExposure',
+                         'FireplaceQu', 'GarageType', 'GarageFinish']
+NUMERICAL_TO_IMPUTE = ['LotFrontage']
 
-NUMERICAL_TO_IMPUTE = 'LotFrontage'
+YEAR_VARIABLE = 'YearRemodAdd'
 
-YEAR_VARIABLE = 'YEarRemodAdd'
-
-# variables to transform
-
+# variables to transofmr
 NUMERICAL_LOG = ['LotFrontage', '1stFlrSF', 'GrLivArea', 'SalePrice']
 
-# variables to encode
 
+# variables to encode
 CATEGORICAL_ENCODE = ['MSZoning', 'Neighborhood', 'RoofStyle',
-                      'MasVnrType', 'BsmtQual', 'BsmtExposure', 'HeatingQC',
-                      'CentralAir', 'KitchenQual', 'FireplaceQu', 'GarageType', 'GarageFinish', 'PaveDrive']
+                      'MasVnrType', 'BsmtQual', 'BsmtExposure',
+                      'HeatingQC', 'CentralAir', 'KitchenQual',
+                      'FireplaceQu', 'GarageType', 'GarageFinish',
+                      'PavedDrive']
+
 
 # selected features for training
-
 FEATURES = ['MSSubClass', 'MSZoning', 'Neighborhood', 'OverallQual',
-            'OverallCond', 'YearRemodAdd', 'RoffStyle', 'MasVnrType',
-            'BsmtQual', 'BsmtExposure', 'HeatingQC', 'CentralAir', '1stFlrSF',
-            'GrLivArea', 'BsmtFullBath', 'KitchenQual', 'Fireplaces', 'FireplaceQu',
-            'GarageType', 'GarageFinish', 'GarageCars', 'PavedDrive', 'LotFrontage']
+            'OverallCond', 'YearRemodAdd', 'RoofStyle', 'MasVnrType',
+            'BsmtQual', 'BsmtExposure', 'HeatingQC', 'CentralAir',
+            '1stFlrSF', 'GrLivArea', 'BsmtFullBath', 'KitchenQual',
+            'Fireplaces', 'FireplaceQu', 'GarageType', 'GarageFinish',
+            'GarageCars', 'PavedDrive', 'LotFrontage']
