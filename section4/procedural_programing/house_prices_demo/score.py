c75397913e5362fbb9a3a622e7bf7b0df0705fe0
jeffresh <manuel.pallanunez@alum.uca.es>
2021-01-25 14:59:08 +0100

    refactor(score): usecorrect imports

--- section4/procedural_programing/house_prices_demo/score.py
+++ section4/procedural_programing/house_prices_demo/score.py
@@ -1,56 +1,55 @@
-from math import sqrt
-from section4.procedural_programing.house_prices_demo.train import X_test
-from .preprocessing_functions import *
-from .config import *
+import preprocessing_functions as pf
+import config
 
-# ======= Scoring pipeline
+# =========== scoring pipeline =========
+
+# impute categorical variables
 
 
 def predict(data):
 
     # impute NA
+    for var in config.CATEGORICAL_TO_IMPUTE:
+        data[var] = pf.impute_na(data, var, replacement='Missing')
 
-    for var in CATEGORICAL_TO_IMPUTE:
-        data[var] = impute_na(data, var, replacement='Missing')
-
-    for var in NUMERICAL_TO_IMPUTE:
-        data[var] = impute_na(
-            data, var, replacement=LOTFRONTAGE_MODE if var == 'LotFrontage' else 'Missing')
+    data[config.NUMERICAL_TO_IMPUTE] = pf.impute_na(data,
+                                                    config.NUMERICAL_TO_IMPUTE,
+                                                    replacement=config.LOTFRONTAGE_MODE)
 
     # capture elapsed time
-
-    data[YEAR_VARIABLE] = elapsed_years(data, YEAR_VARIABLE)
+    data[config.YEAR_VARIABLE] = pf.elapsed_years(data,
+                                                  config.YEAR_VARIABLE, ref_var='YrSold')
 
     # log transform numerical variables
-
-    for var in NUMERICAL_LOG:
-        data[var] = log_transform(data, var)
+    for var in config.NUMERICAL_LOG:
+        data[var] = pf.log_transform(data, var)
 
     # Group rare labels
-    for var in CATEGORICAL_ENCODE:
-        data[var] = remove_rare_labels(data, var, FREQUENT_LABELS[var])
+    for var in config.CATEGORICAL_ENCODE:
+        data[var] = pf.remove_rare_labels(
+            data, var, config.FREQUENT_LABELS[var])
 
     # encode variables
-
-    for var in CATEGORICAL_ENCODE:
-        data[var] = encode_categorical(data, var, ENCODING_MAPPINGS)
+    for var in config.CATEGORICAL_ENCODE:
+        data[var] = pf.encode_categorical(data, var,
+                                          config.ENCODING_MAPPINGS[var])
 
     # scale variables
-
-    data = scale_features(data[FEATURES], OUTPUT_SCALER_PATH)
+    data = pf.scale_features(data[config.FEATURES],
+                             config.OUTPUT_SCALER_PATH)
 
     # make predictions
-
-    predictions = predict(data, OUTPUT_MODEL_PATH)
+    predictions = pf.predict(data, config.OUTPUT_MODEL_PATH)
 
     return predictions
 
-# ================================
+# ======================================
 
 # small test that scripts are working ok
 
 
 if __name__ == '__main__':
+
     from math import sqrt
     import numpy as np
 
@@ -59,24 +58,18 @@ if __name__ == '__main__':
     import warnings
     warnings.simplefilter(action='ignore')
 
-    # load data
-    data = pd.load_data(PATH_TO_DATASET)
-    X_train, X_test, y_train, y_test = divide_train_test(data, TARGET)
+    # Load data
+    data = pf.load_data(config.PATH_TO_DATASET)
+    X_train, X_test, y_train, y_test = pf.divide_train_test(data,
+                                                            config.TARGET)
 
     pred = predict(X_test)
 
     # determine mse and rmse
-
     print('test mse: {}'.format(int(
-        mean_squared_error(y_test, np.exp(pred))
-    )))
-
+        mean_squared_error(y_test, np.exp(pred)))))
     print('test rmse: {}'.format(int(
-        sqrt(mean_squared_error(y_test, np.exp(pred)))
-    )))
-
+        sqrt(mean_squared_error(y_test, np.exp(pred))))))
     print('test r2: {}'.format(
-        r2_score(y_test, np.exp(pred))
-    ))
-
+        r2_score(y_test, np.exp(pred))))
     print()
