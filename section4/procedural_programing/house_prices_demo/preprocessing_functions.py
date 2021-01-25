2778a78f08be577b7a0c26483c8b065938395075
jeffresh <manuel.pallanunez@alum.uca.es>
2021-01-25 14:58:36 +0100

    refactor(preprocessing_functions): improve format and comments

--- section4/procedural_programing/house_prices_demo/preprocessing_functions.py
+++ section4/procedural_programing/house_prices_demo/preprocessing_functions.py
@@ -1,100 +1,81 @@
 import pandas as pd
 import numpy as np
-from sklearn import linear_model
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import Lasso
 
 import joblib
 
-# Individiual pre-processing and training functions
 
+# Individual pre-processing and training functions
+# ================================================
 
 def load_data(df_path):
-    '''
-    Function loads data for training
-    '''
-
+    # Function loads data for training
     return pd.read_csv(df_path)
 
 
 def divide_train_test(df, target):
-    '''
-    Function divides data set in train and test 
-    '''
-
-    X_train, X_test, y_train, y_test = train_test_split(df.drop(target, axis=1),
+    # Function divides data set in train and test
+    X_train, X_test, y_train, y_test = train_test_split(df,
                                                         df[target],
-                                                        test_size=0.2,
+                                                        test_size=0.1,
                                                         random_state=0)
-
     return X_train, X_test, y_train, y_test
 
 
+def impute_na(df, var, replacement='Missing'):
+    # function replaces NA by value entered by user
+    # or by string Missing (default behaviour)
+    return df[var].fillna(replacement)
+
+
 def elapsed_years(df, var, ref_var='YrSold'):
+    # captures difference between a year variable
+    # and a reference variable
+    # (year in which the house was sold by default)
+
     df[var] = df[ref_var] - df[var]
     return df
 
 
 def log_transform(df, var):
+    # apply logarithm transformation to variable
     return np.log(df[var])
 
 
-def extract_cabin_letter(df, var):
-    return df['cabin'].str[0]
+def remove_rare_labels(df, var, frequent_labels):
+    # groups labels that are not in the frequent list into the umbrella
+    # group Rare
+    return np.where(df[var].isin(frequent_labels), df[var], 'Rare')
 
 
-def add_missing_indicator(df, var):
-    return np.where(df[var].isnull(), 1, 0)
+def encode_categorical(df, var, mappings):
+    # replaces strings by numbers using mappings dictionary
+    return df[var].map(mappings)
 
 
-def impute_na(df, var, replacement='Missing'):
-    return df[var].fillna(replacement)
-
-
-def remove_rare_labels(df, var, frequent_labels, replacement='Rare'):
-    return np.where(df[var].isin(frequent_labels), df[var], replacement)
-
-
-def encode_categorical(df, var):
-    df = df.copy()
-    df = pd.concat(
-        [df, pd.get_dummies(df[var], prefix=var, drop_first=True)], axis=1)
-    df.drop(labels=[var], axis=1, inplace=True)
-
-    return df
-
-
-def check_dummy_variables(df, dummy_list):
-    missing_vars = [var for var in dummy_list if var not in df.columns]
-
-    if len(missing_vars) == 0:
-        print('All dummies were added')
-    else:
-        for var in missing_vars:
-            df[var] = 0
-
-    return df
-
-
-def train_scaler(df, ouput_path):
-    scaler = StandardScaler()
+def train_scaler(df, output_path):
+    scaler = MinMaxScaler()
     scaler.fit(df)
-    joblib.dump(scaler, ouput_path)
+    joblib.dump(scaler, output_path)
     return scaler
 
 
-def scale_features(df, output_path):
-    scaler = joblib.load(output_path)
+def scale_features(df, scaler):
+    scaler = joblib.load(scaler)  # with joblib probably
     return scaler.transform(df)
 
 
 def train_model(df, target, output_path):
-    lin_model = LogisticRegression(C=0.0005, random_state=0)
+    # initialise the model
+    lin_model = Lasso(alpha=0.005, random_state=0)
 
+    # train the model
     lin_model.fit(df, target)
 
+    # save the model
     joblib.dump(lin_model, output_path)
 
     return None
