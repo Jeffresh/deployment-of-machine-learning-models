d452ad60aa2fc47717d6344e102700fdf1bd297f
jeffresh <manuel.pallanunez@alum.uca.es>
2021-01-27 17:50:23 +0100

    refactor( pipeline config): fix some typo erros in keys

--- section4/custom_pipeline/config.py
+++ section4/custom_pipeline/config.py
@@ -7,7 +7,7 @@ CATEGORICAL_TO_IMPUTE = ['MasVnrType', 'BsmtQual',
 
 NUMERICAL_TO_IMPUTE = ['LotFrontage']
 
-YEAR_VARIABLE = 'YEarRemodAdd'
+YEAR_VARIABLE = 'YearRemodAdd'
 
 NUMERICAL_LOG = ['LotFrontage', '1stFlrSF', 'GrLivArea', 'SalePrice']
 
@@ -18,7 +18,7 @@ CATEGORICAL_ENCODE = ['MSZoning', 'Neighborhood', 'RoofStyle',
                       'PavedDrive']
 
 FEATURES = ['MSSubClass', 'MSZoning', 'Neighborhood', 'OverallQual',
-            'OverallCond', 'YearRemodAdd', 'RoffStyle', 'MasVnrType',
+            'OverallCond', 'YearRemodAdd', 'RoofStyle', 'MasVnrType',
             'BsmtQual', 'BsmtExposure', 'HeatingQC', 'CentralAir',
             '1stFlrSF', 'GrLivArea', 'BsmtFullBath', 'KitchenQual',
             'Fireplaces', 'FireplaceQu', 'GarageType', 'GarageFinish',
