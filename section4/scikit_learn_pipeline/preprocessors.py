7542707b6c352eaa55a2377bfe890355c426e791
jeffresh <manuel.pallanunez@alum.uca.es>
2021-01-28 17:40:22 +0100

    feature(scikit preprocessors): add Numerical Imputer

--- section4/scikit_learn_pipeline/preprocessors.py
+++ section4/scikit_learn_pipeline/preprocessors.py
@@ -24,3 +24,28 @@ class CategoricalImputer(BaseEstimator, TransformerMixin):
             X[feature] = X[feature].fillna('Missing')
 
         return X
+
+# Numerical missing value imputer
+
+
+class NumericalImputer(BaseEstimator, TransformerMixin):
+
+    def __init__(self, variables=None):
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X, y=None):
+        # persist mode in a dictionary
+        self.imputer_dict_ = {}
+
+        for feature in self.variables:
+            self.imputer_dict_[feature] = X[feature].mode()[0]
+        return X
+
+    def transform(self, X):
+
+        X = X.copy()
+        for feature in self.variables:
+            X[feature].fillna(self.imputer_dict_[feature], inplace=True)
