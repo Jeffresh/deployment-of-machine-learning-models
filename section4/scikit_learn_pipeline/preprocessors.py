05a9b1a4dba0ad49101b20bea1576b5f82e25313
jeffresh <manuel.pallanunez@alum.uca.es>
2021-01-28 17:50:04 +0100

    feat(scikit preprocessors): add rare label categorical encoder

--- section4/scikit_learn_pipeline/preprocessors.py
+++ section4/scikit_learn_pipeline/preprocessors.py
@@ -49,3 +49,38 @@ class NumericalImputer(BaseEstimator, TransformerMixin):
         X = X.copy()
         for feature in self.variables:
             X[feature].fillna(self.imputer_dict_[feature], inplace=True)
+
+
+class RareLabelCategoricalEncoder(BaseEstimator, TransformerMixin):
+
+    def __init__(self, tol=0.05, variables=None):
+
+        self.tol = tol
+
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X, y=None):
+
+        # persist frequent labels in dictionary
+        self.encoder_dict_ = {}
+
+        for var in self.variables:
+            # the encoder will learn the most frequent categories
+            t = pd.Series(X[var].value_counts() / np.float(len(X)))
+            # frequent labels:
+
+            self.encoder_dict_[var] = list(t[t >= self.tol].index)
+
+            return self
+
+    def transform(self, X):
+
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = np.where(X[feature].isin(
+                self.encoder_dict_[feature]), X[feature], 'Rare')
+
+        return X
