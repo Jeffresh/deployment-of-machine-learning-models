5b65921753be97bdf0d0dacadcd16a19062d1fd5
jeffresh <manuel.pallanunez@alum.uca.es>
2021-01-29 11:19:42 +0100

    feat(scikit preprocessors): add drop unecessary features

--- section4/scikit_learn_pipeline/preprocessors.py
+++ section4/scikit_learn_pipeline/preprocessors.py
@@ -166,3 +166,20 @@ class LogTransformer(BaseEstimator, TransformerMixin):
             X[feature] = np.log(X[feature])
 
         return X
+
+
+class DropUnecessaryFeatures(BaseEstimator, TransformerMixin):
+
+    def __init__(self, variables_to_drop=None):
+
+        self.variables = variables_to_drop
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+
+        X = X.drop(self.variables, axis=1)
+
+        return x
