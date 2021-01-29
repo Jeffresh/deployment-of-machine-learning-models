285870fbe4cb12eab335a9a759c57bbbee3bd218
jeffresh <manuel.pallanunez@alum.uca.es>
2021-01-29 10:28:37 +0100

    feat(scikit preprocessors): add categorical encoder

--- section4/scikit_learn_pipeline/preprocessors.py
+++ section4/scikit_learn_pipeline/preprocessors.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+
 from sklearn.base import BaseEstimator, TransformerMixin
 
 # categorical missing value imputer
@@ -78,6 +79,7 @@ class RareLabelCategoricalEncoder(BaseEstimator, TransformerMixin):
 
     def __init__(self, tol=0.05, variables=None):
 
+        # tolerance
         self.tol = tol
 
         if not isinstance(variables, list):
@@ -105,5 +107,39 @@ class RareLabelCategoricalEncoder(BaseEstimator, TransformerMixin):
         for feature in self.variables:
             X[feature] = np.where(X[feature].isin(
                 self.encoder_dict_[feature]), X[feature], 'Rare')
+
+        return X
+
+
+class CategoricalEncoder(BaseEstimator, TransformerMixin):
+
+    def __init__(self, variables=None):
+        if not isinstance(list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X, y):
+        # y != None because wee need it all cases
+        temp = pd.concat([X, y], axis=1)
+        temp.columns = list(X.columns) + ['target']
+
+        # persist transforming dictionary
+        self.encoder_dict_ = {}
+
+        for var in self.variables:
+            t = temp.groupby([var])['target'].mean(
+            ).sort_values(ascending=True).index
+            self.encoder_[var] = {k: i for i, k in enumerate(t, 0)}
+
+        return self
+
+    def transform(self, X):
+        # encode variables
+
+        X = X.copy()
+
+        for feature in self.variables:
+            X[feature] = X[feature].map(self.encoder_dict_[feature])
 
         return X
