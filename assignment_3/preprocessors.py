79bae98ffaac7411543a47c79e63c0e087c20c02
jeffresh <manuel.pallanunez@alum.uca.es>
2021-01-30 18:11:21 +0100

    feat(assignment 3 preprocessors): add transformers and encoders

--- assignment_3/preprocessors.py
+++ assignment_3/preprocessors.py
@@ -8,104 +8,163 @@ from sklearn.base import BaseEstimator, TransformerMixin
 class MissingIndicator(BaseEstimator, TransformerMixin):
 
     def __init__(self, variables=None):
-        pass
-
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
 
     def fit(self, X, y=None):
         # to accommodate sklearn pipeline functionality
-        pass
-
+        return self
 
     def transform(self, X):
         # add indicator
         X = X.copy()
-        pass
 
+        for var in self.variables:
+            X[var + '_NA'] = np.where(X[var].isnull(), 1, 0)
+
+        return X
 
 # categorical missing value imputer
+
+
 class CategoricalImputer(BaseEstimator, TransformerMixin):
 
     def __init__(self, variables=None):
-        pass
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
 
     def fit(self, X, y=None):
         # we need the fit statement to accommodate the sklearn pipeline
-        pass
+        return self
 
     def transform(self, X):
         X = X.copy()
-        pass
+
+        X[self.variables] = X[self.variables].fillna('Missing')
+
+        return X
 
 
 # Numerical missing value imputer
 class NumericalImputer(BaseEstimator, TransformerMixin):
 
     def __init__(self, variables=None):
-        pass
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
 
     def fit(self, X, y=None):
         # persist mode in a dictionary
         self.imputer_dict_ = {}
-        pass
+        for var in self.variables:
+            self.imputer_dict_[var] = X[var].mode()[0]
+
+        return self,
 
     def transform(self, X):
 
         X = X.copy()
-        pass
+
+        for var in self.variables:
+            X[var].fillna(self.imputer_dict_[var], inplace=True)
+
+        return X
 
 
 # Extract first letter from string variable
 class ExtractFirstLetter(BaseEstimator, TransformerMixin):
 
     def __init__(self, variables=None):
-        pass
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
 
     def fit(self, X, y=None):
         # we need this step to fit the sklearn pipeline
-        pass
+        return self
 
     def transform(self, X):
         X = X.copy()
-        pass
+
+        for variable in self.variables:
+            X[variable] = X[variable].str[0]
+
+        return X
 
 # frequent label categorical encoder
+
+
 class RareLabelCategoricalEncoder(BaseEstimator, TransformerMixin):
 
     def __init__(self, tol=0.05, variables=None):
-        pass
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+        self.tol = tol
 
     def fit(self, X, y=None):
 
         # persist frequent labels in dictionary
         self.encoder_dict_ = {}
-        pass
+
+        for variable in self.variables:
+            prop = X[variable].value_counts() / len(X)
+            self.encoder_dict_[variable] = prop[prop >=
+                                                self.tol].index.tolists()
+        return self
 
     def transform(self, X):
         X = X.copy()
-        pass
+
+        for variable in self.variables:
+            X[variable] = np.where(X[variable].isin(
+                self.encoder_dict_[variable]), X[variable], 'Rare')
+
+        return X
 
 
 # string to numbers categorical encoder
 class CategoricalEncoder(BaseEstimator, TransformerMixin):
 
     def __init__(self, variables=None):
-        pass
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
 
     def fit(self, X, y=None):
 
         # HINT: persist the dummy variables found in train set
-        self.dummies = pd.get_dummies(X[self.variables], drop_first=True).columns
-        
+        self.dummies = pd.get_dummies(
+            X[self.variables], drop_first=True).columns
+
         return self
 
     def transform(self, X):
         # encode labels
         X = X.copy()
         # get dummies
-        
+        X = pd.concat(
+            [X, pd.get_dummies(X[self.variables], drop_first=True)], axis=1)
+
         # drop original variables
+
+        X.drop(labels=self.variables, axis=1, inplace=True)
 
         # add missing dummies if any
 
+        missing_vars = [var for var in self.dummies if var not in X.columns]
+
+        if not missing_vars:
+            for var in missing_vars:
+                X[var] = 0
 
         return X
