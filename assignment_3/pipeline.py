2f0e2cd42642b33c22f8029f5997fb9dd9ac78d2
jeffresh <manuel.pallanunez@alum.uca.es>
2021-01-30 19:01:06 +0100

    feat(assignment 3 pipeline): add pipeline

--- assignment_3/pipeline.py
+++ assignment_3/pipeline.py
@@ -1,3 +1,4 @@
+from assignment_3.preprocessors import MissingIndicator
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
@@ -9,4 +10,24 @@ import config
 titanic_pipe = Pipeline(
     # complete with the list of steps from the preprocessors file
     # and the list of variables from the config
-    )
+    ('categorical_imputer', pp.CategoricalImputer(
+        variables=config.CATEGORICAL_VARS)),
+
+    ('missing indicator', pp.MissingIndicator(
+        variables=config.NUMERICAL_VARS)),
+
+    ('numerical_imputer', pp.NumericalImputer(
+        variables=config.NUMERICAL_VARS)),
+
+    ('cabin_variable', pp.ExtractFirstLetter(variables=config.CABIN)),
+
+    ('rare_label_encoder', pp.RareLabelCategoricalEncoder(
+        tol=0.05, variables=config.CATEGORICAL_VARS)),
+
+    ('categorical_encoder', pp.CategoricalEncoder(
+        variables=config.CATEGORICAL_VARS)),
+
+    ('scaler', StandardScaler()),
+
+    ('Linear_model', LogisticRegression(C=0.0005, random_state=0))
+)
