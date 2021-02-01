22ad109cfd7f3cf7b1d500fb1819ebb61dded2f5
jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-01 15:36:09 +0100

    refactor(section 6 data management) relocate code of train pipeline
    add new data management

--- section6/regression_model/regression_model/train_pipeline.py
+++ section6/regression_model/regression_model/train_pipeline.py
@@ -1,20 +1,9 @@
 import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
-import joblib
 
 from regression_model import pipeline
+from regression_model.processing.data_management import load_dataset, save_pipeline
 from regression_model.config import config
-
-
-def save_pipeline(*, pipeline_to_persist) -> None:
-    """Persist the pipeline."""
-
-    save_file_name = 'regression_model.pkl'
-    save_path = config.TRAINED_MODEL_DIR / save_file_name
-    joblib.dump(pipeline_to_persist, save_path)
-
-    print('saved_pipeline')
 
 
 def run_training() -> None:
@@ -22,7 +11,7 @@ def run_training() -> None:
 
     # read training data
 
-    data = pd.read_csv(config.DATASET_DIR / config.TRAINING_DATA_FILE)
+    data = load_dataset(file_name=config.TRAINING_DATA_FILE)
 
     # divide train and test
 
