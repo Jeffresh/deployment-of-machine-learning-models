b5dfee7047c56184a3463ceeed32c7d97ecdfea5
jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-01 13:47:49 +0100

    fix(section6 config): typo on dataset dir
    datasets -> dataset

--- section6/regression_model/regression_model/config/config.py
+++ section6/regression_model/regression_model/config/config.py
@@ -5,7 +5,7 @@ import regression_model
 
 PACKAGE_ROOT = pathlib.Path(regression_model.__file__).resolve().parent
 TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
-DATASET_DIR = PACKAGE_ROOT / "datasets"
+DATASET_DIR = PACKAGE_ROOT / "dataset"
 
 # data
 TESTING_DATA_FILE = "test.csv"
