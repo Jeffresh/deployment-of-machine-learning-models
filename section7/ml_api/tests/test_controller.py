1bdca433ff26cb119df5471be902af782fb1d567
jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-03 10:10:53 +0100

    test(section 7 tests controller): add test for version endpoint

--- section7/ml_api/tests/test_controller.py
+++ section7/ml_api/tests/test_controller.py
@@ -1,6 +1,7 @@
 from regression_model.config import config as model_config
 from regression_model.preprocessing.data_management import load_dataset
 from regression_model import __version__ as _version
+from api import __version__ as api_version
 
 import json
 import math
@@ -36,3 +37,14 @@ def test_prediction_endpoint_returns_prediction(flask_test_client):
     response_version = response_json['version']
     assert math.ceil(prediction) == 112476
     assert response_version == _version
+
+
+def test_version_endpoint_returns_version(flask_test_client):
+    # When
+    response = flask_test_client.get('/version')
+
+    # Then
+    assert response.status_code == 200
+    response_json = json.loads(response.data)
+    assert response_json['model_version'] == _version
+    assert response_json['api_version'] == api_version
