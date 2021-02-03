3c1da03363e78d9c1df3a35554f152a55490c5d4
jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-03 10:09:21 +0100

    feat(section 7 controller): add version endpoint

--- section7/ml_api/api/controller.py
+++ section7/ml_api/api/controller.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request, jsonify
 from regression_model.predict import make_prediction
+from regression_model import __version__ as _version
 
 from api.config import get_logger
+from api import __version__ as api_version
 
 _logger = get_logger(logger_name=__name__)
 
@@ -29,3 +31,10 @@ def predict():
 
         return jsonify({'predictions': predictions,
                         'version': version})
+
+
+@prediction_app.route('/version', methods=['GET'])
+def version():
+    if request.method == 'GET':
+        return jsonify({'model_version': _version,
+                        'api_version': api_version})
