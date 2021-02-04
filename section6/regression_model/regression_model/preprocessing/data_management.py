f98ba7796af26c2d03afc311807267ee88d4c510
jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-04 09:53:39 +0100

    feat(regression model data management): add location info

--- section6/regression_model/regression_model/preprocessing/data_management.py
+++ section6/regression_model/regression_model/preprocessing/data_management.py
@@ -42,7 +42,7 @@ def save_pipeline(*, pipeline_to_persist) -> None:
 
     remove_old_pipelines(files_to_keep=save_file_name)
     joblib.dump(pipeline_to_persist, save_path)
-    _logger.info(f"saved pipeline: {save_file_name}")
+    _logger.info(f"saved pipeline: {save_file_name} at {save_path}")
 
 
 def load_pipeline(*, file_name: str) -> Pipeline:
