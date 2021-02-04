28b17227f27d315ef68a72a7d1b7dbcef7f15c41
Jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-04 21:44:12 +0100

    refactor: update url dataset

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p .
-unzip house-prices-advanced-regression-techniques.zip -d section6/regression_model/regression_model/datasets/
+unzip house-prices-advanced-regression-techniques.zip -d section6/regression_model/regression_model/dataset/
 
 
