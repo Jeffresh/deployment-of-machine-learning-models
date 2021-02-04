ddc82f0e061083e0add89b956a979c7353ff6f94
jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-04 09:28:32 +0100

    refactor(package setup): add metadata package

--- section6/regression_model/setup.py
+++ section6/regression_model/setup.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import io
 import os
 from pathlib import Path
 
 from setuptools import find_packages, setup
 
-# Package meta-data.
 
+# Package meta-data.
 NAME = 'regression_model'
 DESCRIPTION = 'Train and deploy regression model.'
 URL = 'your github project'
@@ -13,40 +16,38 @@ EMAIL = 'your_email@email.com'
 AUTHOR = 'Your name'
 REQUIRES_PYTHON = '>=3.6.0'
 
+
 # What packages are required for this module to be executed?
-
-
 def list_reqs(fname='requirements.txt'):
     with open(fname) as fd:
         return fd.read().splitlines()
 
-# The rest you shouldn't have to touch too much
-# ---------------------------------------------
-# Except, perhaps the License and Trove Classifiers
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
 # If you do change the License, remember to change the
 # Trove Classifier for that!
 
-
 here = os.path.abspath(os.path.dirname(__file__))
 
-# Import tthe README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in our MANIFEST.in file
-
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
     with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
         long_description = '\n' + f.read()
-
 except FileNotFoundError:
     long_description = DESCRIPTION
+
 
 # Load the package's __version__.py module as a dictionary.
 ROOT_DIR = Path(__file__).resolve().parent
 PACKAGE_DIR = ROOT_DIR / NAME
-
 about = {}
 with open(PACKAGE_DIR / 'VERSION') as f:
     _version = f.read().strip()
     about['__version__'] = _version
+
 
 # Where the magic happens:
 setup(
@@ -67,15 +68,14 @@ setup(
     license='MIT',
     classifiers=[
         # Trove classifiers
-        # Full list : https://pypi.python.org/pypi?%3Action=list_classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: MIT License',
-        'Programing Language :: Python',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: Pypy'
-
+        'Programming Language :: Python :: Implementation :: PyPy'
     ],
 )
