9fc6cbbfdf1d1d125657a0584f17e7572ea7d74c
Jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-04 21:49:45 +0100

    fix: url dataset script

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-kaggle competitions download -c house-prices-advanced-regression-techniques -p .
-unzip house-prices-advanced-regression-techniques.zip -d section6/regression_model/regression_model/dataset/
+kaggle competitions download -c house-prices-advanced-regression-techniques -p section6/regression_model/regression_model/dataset/
 
 
