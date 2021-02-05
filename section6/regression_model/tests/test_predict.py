412b10f9b5f90b046498d6ce2133ede929d4ec8a
Jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-05 20:58:43 +0100

    refactor(section 9 test predict): improvements

--- section6/regression_model/tests/test_predict.py
+++ section6/regression_model/tests/test_predict.py
@@ -7,10 +7,10 @@ from regression_model.preprocessing.data_management import load_dataset
 def test_make_single_prediction():
     # Given
     test_data = load_dataset(file_name='test.csv')
-    single_test_json = test_data[0:1]
+    single_test_input = test_data[0:1]
 
     # When
-    subject = make_prediction(input_data=single_test_json)
+    subject = make_prediction(input_data=single_test_input)
 
     # Then
     assert subject is not None
