ec55b9dc3dd19d1fa0ac799678b682fca7fa39e4
jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-08 18:15:52 +0100

    fix: utf8 codification error

--- scripts/fetch_kaggle_large_dataset.sh
+++ scripts/fetch_kaggle_large_dataset.sh
@@ -7,5 +7,5 @@ kaggle datasets download -d $TRAINING_DATA_URL -p section13/neural_network_model
 unzip section13/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset.zip -d section13/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset && \
 echo $TRAINING_DATA_URL 'retrieved on:' $NOW > section13/neural_network_model/neural_network_model/datasets/training_data_reference.txt && \
 mkdir -p "./section13/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherds Purse"  && \
-mv -v "./section13/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherd's Purse/"* "./section13/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherds Purse"
-rm -rf "./section13/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherd's Purse"
+mv -v "./section13/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherd’s Purse/"* "./section13/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherds Purse"
+rm -rf "./section13/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherd’s Purse"
