f5aeaddc96d7c04ceca35ae00b879b53d289a3c5
jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-08 14:56:28 +0100

    feat(section 13 scripts): add script to fetch kaggle dataset

---
+++ scripts/fetch_kaggle_large_dataset.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+TRAINING_DATA_URL="vbookshelf/v2-plant-seedlings-dataset"
+NOW=$(date)
+
+kaggle datasets download -d $TRAINING_DATA_URL -p section13/neural_network_model/neural_network_model/datasets/ && \
+unzip section13/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset.zip -d section13/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset && \
+echo $TRAINING_DATA_URL 'retrieved on:' $NOW > section13/neural_network_model/neural_network_model/datasets/training_data_reference.txt && \
+mkdir -p "./section13/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherds Purse"  && \
+mv -v "./section13/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherd’s Purse/"* "./section13/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherds Purse"
+rm -rf "./section13/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherd’s Purse
